f6f264189e7a0de6617cf2ba48435d6e50a79e8d
Misael Aguayo <aguayo.misael@gmail.com>
2024-09-15 10:05:53 -0500

    Add text handling

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
   pkgs = import nixpkgs { config = {}; overlays = []; };
-  libraries = with pkgs; [ SDL2 ];
+  libraries = with pkgs; [ SDL2 SDL2_ttf ];
 in
 
 pkgs.mkShellNoCC {
---
+++ src/term.rs
@@ -0,0 +1,10 @@
+pub struct State {
+    commands: Vec<String>
+}
+
+pub struct Terminal {
+   frontend: State,
+   backend: State
+}
+
+
