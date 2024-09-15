5a8e727d524964d66ce8a8f44ae0861dc9eb2b3a
Misael Aguayo <aguayo.misael@gmail.com>
2024-09-15 09:12:52 -0500

    Create nix shell file

---
+++ shell.nix
@@ -0,0 +1,11 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+  libraries = with pkgs; [ SDL2 ];
+in
+
+pkgs.mkShellNoCC {
+  shellHook = ''
+    export LIBRARY_PATH="${pkgs.lib.makeLibraryPath libraries}"
+  '';
+}
