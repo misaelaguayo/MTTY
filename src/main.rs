ecc4ebd9f9a67e0a28a17e5fe9788d6f8af8129d
Misael Aguayo <aguayo.misael@gmail.com>
2024-09-15 10:05:30 -0500

    Create black box

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ pub fn main() -> Result<(), String> {
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rust-sdl2 demo: Video", 800, 600)
+        .window("MTTY", 800, 600)
         .position_centered()
         .opengl()
         .build()
@@ -25,7 +25,7 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
@@ -45,7 +45,6 @@ pub fn main() -> Result<(), String> {
         canvas.clear();
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
     }
 
     Ok(())
