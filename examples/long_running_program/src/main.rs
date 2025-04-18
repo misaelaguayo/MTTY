7cadf0955cdf76d55d6005c4aede5aa363652ef6
Misael Aguayo <aguayo.misael@gmail.com>
2025-04-18 11:30:28 -0500

    Clean up

--- examples/long_running_program/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    // a long running process
-    // useful for testing that the shell can run processes in the background
-
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    println!("Long running process finished!");
-}
