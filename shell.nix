bcae19719cdde7097865c73c291ae8e9dae69189
Misael Aguayo <aguayo.misael@gmail.com>
2025-04-27 09:56:57 -0500

    Add test coverage

--- shell.nix
+++ shell.nix
@@ -1,12 +1,24 @@
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
   pkgs = import nixpkgs { config = {}; overlays = []; };
-  libraries = with pkgs; [ SDL2 SDL2_ttf ];
+  bin = with pkgs; [ libllvm ];
 in
 
 pkgs.mkShellNoCC {
-  nativeBuildInputs = with pkgs.buildPackages; [ pkg-config fontconfig ];
+  nativeBuildInputs = with pkgs.buildPackages; [ clang ];
+  packages = with pkgs; [
+    grcov
+    lcov
+    cargo
+  ];
   shellHook = ''
-    export LIBRARY_PATH="${pkgs.lib.makeLibraryPath libraries}"
+    export CARGO_INCREMENTAL=0
+    export RUSTFLAGS="-Cinstrument-coverage"
+    export LLVM_PROFILE_FILE="cargo-test-%p-%m.profraw"
+    export BIN_PATH="${pkgs.lib.makeBinPath bin}"
   '';
+
+  # generating coverage reports
+  # cargo test
+  # grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html --llvm-path=$BIN_PATH
 }
