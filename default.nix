aaf14cb0bd217f2530b9a8d24b42ad1c6522f7ab
Misael Aguayo <aguayo.misael@gmail.com>
2025-07-12 12:14:51 -0500

    Add flake

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{ nixpkgs }:
 
-nixpkgs.rustPlatform.buildRustPackage {
-  pname = "MTTY";
-  version = "v0.1";
-  src = ./.;
-  cargoHash = "sha256-VX/7Gn3dO5OIj1Ktm78dWOKwKW0+MS/aFJMYMFDYj+I=";
-  buildInputs = 
-    [ nixpkgs.SDL2 nixpkgs.SDL2_ttf nixpkgs.openssl nixpkgs.pkg-config nixpkgs.libiconv nixpkgs.darwin.apple_sdk.frameworks.AppKit ];
+nixpkgs.mkShell {
+  buildInputs = with nixpkgs; [
+    cargo
+    rustc
+    nixpkgs-fmt
+  ];
 }
+
