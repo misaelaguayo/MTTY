152d5cc7b53c0b21efa13c422dbe64dd2dcf513b
Misael Aguayo <aguayo.misael@gmail.com>
2025-07-16 20:34:06 -0500

    Add flake for using iced

--- default.nix
+++ default.nix
@@ -4,7 +4,28 @@ nixpkgs.mkShell {
   buildInputs = with nixpkgs; [
     cargo
     rustc
+    rustup
     nixpkgs-fmt
+    glib
+    gtk3
+    libglvnd
+    libxkbcommon
+    xorg.libX11
   ];
+
+  nativeBuildInputs = with nixpkgs; [
+    pkg-config
+  ];
+
+  shellHook = ''
+    export RUSTFLAGS="-Cinstrument-coverage";
+    export RUSTFLAGS="-Ccodegen-units=1 -Copt-level=0 -Coverflow-checks=off -Cpanic=abort";
+    export RUSTDOCFLAGS="-Cpanic=abort";
+    export LLVM_PROFILE_FILE="coverage.profraw";
+    export CARGO_INCREMENTAL=0;
+    export ICED_BACKEND="tiny-skia";
+  '';
+
+  LD_LIBRARY_PATH = "${nixpkgs.libxkbcommon}/lib";
 }
 
