7078feace6e9cc6d86fe946615ae55954cd504df
Misael Aguayo <aguayo.misael@gmail.com>
2026-01-12 18:00:21 -0600

    Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -13,8 +13,62 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         fenixPkgs = fenix.packages.${system}.complete;
+        isLinux = pkgs.stdenv.isLinux;
+
+        commonLibs = with pkgs; [
+          libGL
+          libxkbcommon
+          fontconfig
+          freetype
+          vulkan-loader
+          libglvnd
+          mesa
+        ];
+
+        waylandLibs = with pkgs; [
+          wayland
+        ];
+
+        x11Libs = with pkgs; [
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+        ];
+
+        mkShell = { graphicsLibs }: pkgs.mkShell {
+          buildInputs = with pkgs; [
+            fenixPkgs.cargo
+            fenixPkgs.clippy
+            fenixPkgs.rustc
+            fenixPkgs.rustfmt
+            fenixPkgs.rust-analyzer
+
+            nixpkgs-fmt
+            llvm
+
+            cargo-flamegraph
+            cargo-tarpaulin
+            cargo-bundle
+          ] ++ pkgs.lib.optionals isLinux (commonLibs ++ graphicsLibs);
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+          ];
+
+          shellHook = ''
+            export RUST_SRC_PATH=${fenixPkgs.rust-src}/lib/rustlib/src/rust/library
+          '' + pkgs.lib.optionalString isLinux ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (commonLibs ++ graphicsLibs)}:$LD_LIBRARY_PATH
+            export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/gfxstream_vk_icd.x86_64.json:/usr/share/vulkan/icd.d/lvp_icd.x86_64.json
+          '';
+        };
       in
       {
-        devShells.default = pkgs.callPackage ./default.nix { nixpkgs = pkgs; fenixPkgs = fenixPkgs; };
+        devShells = {
+          default = mkShell { graphicsLibs = waylandLibs; };
+          wayland = mkShell { graphicsLibs = waylandLibs; };
+          x11 = mkShell { graphicsLibs = x11Libs; };
+        };
       });
 }
